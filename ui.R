94c0bc954d1899efa5ca1f8eeae3de633ea6eec2
ngoishi <43993679+ngoishi@users.noreply.github.com>
2018-10-18 18:07:20 +0900

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,25 +1,44 @@
-library(shiny)
-library(shinythemes)
-library(shinydashboard)
-library(readr)
+library(markdown)
 
-# Define UI for application that plots random distributions 
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel(.libPaths()),
-  
-  # Sidebar with a slider input for number of observations
-  sidebarPanel(
-    sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+navbarPage("Navbar!",
+  tabPanel("Plot",
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons("plotType", "Plot type",
+          c("Scatter"="p", "Line"="l")
+        )
+      ),
+      mainPanel(
+        plotOutput("plot")
+      )
+    )
   ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("distPlot", height=250)
+  tabPanel("Summary",
+    verbatimTextOutput("summary")
+  ),
+  navbarMenu("More",
+    tabPanel("Table",
+      DT::dataTableOutput("table")
+    ),
+    tabPanel("About",
+      fluidRow(
+        column(6,
+          includeMarkdown("about.md")
+        ),
+        column(3,
+          img(class="img-polaroid",
+            src=paste0("http://upload.wikimedia.org/",
+            "wikipedia/commons/9/92/",
+            "1919_Ford_Model_T_Highboy_Coupe.jpg")),
+          tags$small(
+            "Source: Photographed at the Bay State Antique ",
+            "Automobile Club's July 10, 2005 show at the ",
+            "Endicott Estate in Dedham, MA by ",
+            a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
+              "User:Sfoskett")
+          )
+        )
+      )
+    )
   )
-))
+)
