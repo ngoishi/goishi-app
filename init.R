d9b3482e18f0e9d04e022530541fea634a718cd3
ngoishi <43993679+ngoishi@users.noreply.github.com>
2018-10-18 19:07:25 +0900

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shinythemes","shinydashboard","shiny","readr","DT")
+my_packages = c("shinythemes","shinydashboard","shiny","ggplot2","choroplethr","leaflet","dplyr","scales")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
